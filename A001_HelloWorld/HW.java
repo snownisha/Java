59b2722d32eb8e78fb8c4f8f67f7f7002c60cd94
snownisha <snownisha123@gmail.com>
2022-03-12 21:29:58 +0100

    First java program with comments

--- A001_HelloWorld/HW.java
+++ A001_HelloWorld/HW.java
@@ -1,7 +1,15 @@
+/** we cannot execute the program without Package name. Package is a folder which is used
+ * to group the classes.
+ */
 package A001_HelloWorld;
 
 public class HW {
+    /** public static void main (String[]args)- public is an access modifier for the method & this
+     * method can be called directly from outside the class.
+     */
     public static void main (String[]args){
+        /**  System.out.println  is used to print the statement.
+         */
         System.out.println ("Hello World");
     }
 }
