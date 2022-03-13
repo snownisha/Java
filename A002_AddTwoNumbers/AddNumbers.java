f911aea86c76535e05e29a05045ed62d0e76dc37
snownisha <snownisha123@gmail.com>
2022-03-13 19:55:20 +0100

    Sum of Two Numbers

---
+++ A002_AddTwoNumbers/AddNumbers.java
@@ -0,0 +1,12 @@
+package A002_AddTwoNumbers;
+
+public class AddNumbers {
+    public static void main (String[]args){
+        int num1=5;
+        int num2=8;
+        int sum;
+        sum= num1+num2;
+
+        System.out.println ("Sum of two number is" + sum);
+    }
+}
