a7ea6e5739ff58654c2403173d34aef1b67f98df
snownisha <snownisha123@gmail.com>
2022-03-13 20:04:14 +0100

    Basic Calculations

---
+++ A003_BasicCalculations/BasicCalculations.java
@@ -0,0 +1,18 @@
+package A003_BasicCalculations;
+
+public class BasicCalculations {
+    public static void main (String[]args){
+        int num1=50;
+        int num2=8;
+        int Diff;
+        int Prod;
+        int Div;
+        Diff= num1-num2;
+        Prod= num1*num2;
+        Div=num1/num2;
+
+        System.out.println ("Difference between the two number is" + Diff);
+        System.out.println ("Product of two number is" + Prod);
+        System.out.println ("Division of two number is" + Div);
+    }
+}
