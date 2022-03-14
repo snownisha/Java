428248b0a74a687ede154c5649761fed19e9f938
snownisha <snownisha123@gmail.com>
2022-03-14 23:48:26 +0100

    Initilizing the variable

---
+++ A008_InitilizingTheVariables/Test.java
@@ -0,0 +1,9 @@
+package A008_InitilizingTheVariables;
+
+public class Test {
+    public static void main(String[] args) {
+        int num1;
+        num1 = 50;
+        System.out.println(num1);
+    }
+}
