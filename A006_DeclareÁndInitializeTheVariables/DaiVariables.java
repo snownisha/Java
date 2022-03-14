7ed161e45e3c9f620096d65ae264f7f673c5ea10
snownisha <snownisha123@gmail.com>
2022-03-14 23:44:20 +0100

    Declare andInitializ the Variables

--- A006_DeclareÁndInitializeTheVariables/DaiVariables.java
+++ A006_DeclareÁndInitializeTheVariables/DaiVariables.java
@@ -6,7 +6,7 @@ public class DaiVariables {
 
         int x=5;   /** possible ways to declare & initialize the variables */
         int y=x;    /** possible ways to declare & initialize the variables */
-        String str= "1998";   /** possible ways to declare & initialize the variables */
+        String str= "2000git";   /** possible ways to declare & initialize the variables */
         int a =5; int b=8; int c=10;   /** initializing the variables of same type with different values*/
 
 
