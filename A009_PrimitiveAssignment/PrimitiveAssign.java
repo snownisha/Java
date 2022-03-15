739a18416127856b1f0c3e1c12fd2ec9af61358e
snownisha <snownisha123@gmail.com>
2022-03-15 22:23:45 +0100

    Primitive Assignment

---
+++ A009_PrimitiveAssignment/PrimitiveAssign.java
@@ -0,0 +1,17 @@
+package A009_PrimitiveAssignment;
+
+public class PrimitiveAssign {
+    public static void main(String[] args) {
+        int i;
+        long l;
+        float f;
+        double d;
+
+        byte x =5;
+        short z=100;
+
+        i=x;
+
+        System.out.println(i);
+    }
+}
