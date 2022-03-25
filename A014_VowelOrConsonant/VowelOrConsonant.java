a09a437ff5293a483e9da70e07692191a70d4507
snownisha <snownisha123@gmail.com>
2022-03-25 13:58:46 +0100

    Vowel or Consonant

---
+++ A014_VowelOrConsonant/VowelOrConsonant.java
@@ -0,0 +1,14 @@
+package A014_VowelOrConsonant;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+
+        char ch = 's';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
