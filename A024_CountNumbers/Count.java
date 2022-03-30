aa7787302dce6bcda8eff6a45c821e91fca1c0ac
snownisha <snownisha123@gmail.com>
2022-03-30 23:19:06 +0200

    Count Numbers

---
+++ A024_CountNumbers/Count.java
@@ -0,0 +1,10 @@
+package A024_CountNumbers;
+
+public class Count {
+    public static void main(String[] args) {
+        int count = 0, number = 19980508;
+            for (;number != 0; number /= 10, ++count) {
+            }
+            System.out.println("Number of digits: " + count);
+        }
+    }
