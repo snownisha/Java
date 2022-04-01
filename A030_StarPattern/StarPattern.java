bb2858896945f6d71504d6b6ac684a680950e8e1
snownisha <snownisha123@gmail.com>
2022-04-01 23:52:47 +0200

    Star Pattern

---
+++ A030_StarPattern/StarPattern.java
@@ -0,0 +1,17 @@
+package A030_StarPattern;
+
+public class StarPattern {
+    public static void main(String args[]) {
+        {
+            int n = 5;
+            //Outer Loop for number of Rows
+            for (int i = 0; i < n; i++) {
+                // printing '*' in each column.
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
