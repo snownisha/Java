3f6b1a99157291535dc25145f9f805b7192843ab
snownisha <snownisha123@gmail.com>
2022-04-02 00:02:38 +0200

    ASCII Value

---
+++ A031_ASCIIValue/ASCII.java
@@ -0,0 +1,18 @@
+package A031_ASCIIValue;
+
+public class ASCII {
+    public static void main(String[] args)
+    {
+        char ch1 = 's';
+        char ch2 = 'n';
+        char ch3 = 'o';
+
+        int asciival1 = ch1;
+        int asciival2 = ch2;
+        int asciival3 = ch3;
+        System.out.println("The ASCII value of " + ch1 + " is: " + asciival1);
+        System.out.println("The ASCII value of " + ch2 + " is: " + asciival2);
+        System.out.println("The ASCII value of " + ch2 + " is: " + asciival3);
+    }
+}
+
