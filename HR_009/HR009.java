7942cb809f7cdcf7d9b91ee2f584a3f08eed21e8
snownisha <snownisha123@gmail.com>
2022-04-04 23:33:15 +0200

    Java Static Initializer Block

---
+++ HR_009/HR009.java
@@ -0,0 +1,17 @@
+package HR_009;
+
+import java.util.Scanner;
+
+public class HR009 {
+        static int B,H;
+        static boolean flag = true;
+        static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if(B<=0 || H<=0){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+        }
+    }
+}
