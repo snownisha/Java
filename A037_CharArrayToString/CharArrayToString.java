6c3705d3e143793db26482a35075598ac8372762
snownisha <snownisha123@gmail.com>
2022-04-06 00:06:59 +0200

    Char Array to String

---
+++ A037_CharArrayToString/CharArrayToString.java
@@ -0,0 +1,9 @@
+package A037_CharArrayToString;
+
+public class CharArrayToString {
+    public static void main(String[] args) {
+        char[] ch = {'S', 'n', 'o', 'w', ' ','N', 'i', 's', 'h', 'a' };
+        String st2 = new String(ch);
+        System.out.println(st2);
+    }
+}
