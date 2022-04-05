6fbffb85499f09193f895a8adc62f71b47b6140d
snownisha <snownisha123@gmail.com>
2022-04-05 23:59:45 +0200

    String to Character

---
+++ A036_StringtoCharacter/StringtoCharacter.java
@@ -0,0 +1,11 @@
+package A036_StringtoCharacter;
+
+import java.util.Arrays;
+
+public class StringtoCharacter {
+    public static void main(String[] args) {
+        String st = "Snow Nisha Amala Doss";
+        char[] chars = st.toCharArray();
+        System.out.println(Arrays.toString(chars));
+    }
+}
