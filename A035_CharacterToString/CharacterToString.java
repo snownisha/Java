c08a5be42ea4f3e7c75be54248e8d920a8002272
snownisha <snownisha123@gmail.com>
2022-04-05 23:47:56 +0200

    Character to String

---
+++ A035_CharacterToString/CharacterToString.java
@@ -0,0 +1,9 @@
+package A035_CharacterToString;
+
+public class CharacterToString {
+    public static void main(String[] args) {
+        char ch = 'c';
+        String st = Character.toString(ch);
+        System.out.println("The string is: " + st);
+    }
+}
