286eb0375d9062b0172e66d4e7a710942ec09183
snownisha <snownisha123@gmail.com>
2022-04-15 04:49:06 +0200

    Bitwise Operators

---
+++ A045_BitwiseOperators/Bitwise.java
@@ -0,0 +1,13 @@
+package A045_BitwiseOperators;
+
+public class Bitwise {
+        public static void main (String args[]) {
+            int a = 5, b = 10;
+
+            System.out.println(a&b);
+            System.out.println(a|b);
+            System.out.println(a^b);
+            System.out.println(~a);
+        }
+    }
+
