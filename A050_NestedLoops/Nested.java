9cd636cfa79bc5fb0bb6955728babc2b45901987
snownisha <snownisha123@gmail.com>
2022-04-15 23:28:48 +0200

    Star Pattern

---
+++ A050_NestedLoops/Nested.java
@@ -0,0 +1,15 @@
+package A050_NestedLoops;
+
+public class Nested {
+    public static void main (String args[]){
+        int row=3;
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
