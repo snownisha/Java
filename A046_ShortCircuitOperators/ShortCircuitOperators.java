00f643f5840212224a0c7ddd6668991229eaf34f
snownisha <snownisha123@gmail.com>
2022-04-15 05:03:39 +0200

    Short Circuit Operators

---
+++ A046_ShortCircuitOperators/ShortCircuitOperators.java
@@ -0,0 +1,9 @@
+package A046_ShortCircuitOperators;
+
+public class ShortCircuitOperators {
+    public static void main (String args[]){
+        int a=20;int b=30;
+        System.out.println((a>10) && (b<40));
+        System.out.println((a>50) || (b<19));
+    }
+}
