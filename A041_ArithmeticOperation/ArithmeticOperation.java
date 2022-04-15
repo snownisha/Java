9db41477b3f5c451be362ce85487abc09e1ad76d
snownisha <snownisha123@gmail.com>
2022-04-15 03:58:24 +0200

    Arithmetic Operations

---
+++ A041_ArithmeticOperation/ArithmeticOperation.java
@@ -0,0 +1,26 @@
+package A041_ArithmeticOperation;
+
+import java.util.Scanner;
+
+public class ArithmeticOperation {
+    public static void main (String args []){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Num1");
+        int Num1= scanner.nextInt();
+        System.out.println("Enter Num2");
+        int Num2= scanner.nextInt();
+
+        int Add = Num1+Num2;
+        int Sub = Num1-Num2;
+        int Mul = Num1*Num2;
+        int Div = Num1/Num2;
+        int Mod = Num1%Num2;
+
+        System.out.println("Sum of Number is "  + Add );
+        System.out.println("Sub of Number is "  + Sub);
+        System.out.println("Mul of Number is "  + Mul);
+        System.out.println("Div of Number is "  + Div);
+        System.out.println("Mod of Number is "  + Mod);
+
+    }
+}
