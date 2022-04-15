b6656ea31ffd55be7a6014ccdd57dad0a341a241
snownisha <snownisha123@gmail.com>
2022-04-15 04:44:21 +0200

    Relational Operators

---
+++ A044_RelationalOperators/RelationalOperators.java
@@ -0,0 +1,12 @@
+package A044_RelationalOperators;
+
+public class RelationalOperators {
+    public static void main (String args[]) {
+        int a = 5, b = 10;
+
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+    }
+    }
