56c97a2ac385a7e2b218a1b2013f7300b8f4fe89
snownisha <snownisha123@gmail.com>
2022-04-15 22:55:43 +0200

    Print odd Numbers

---
+++ Loops/OddNumbers.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class OddNumbers {
+    public static void main (String args[]){
+        int n=100;
+        for (int i=1; i<=n;i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
