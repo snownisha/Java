0fbbf83e44faf7c1adc00873e38ba8a92fad242c
snownisha <snownisha123@gmail.com>
2022-04-16 08:49:51 +0200

    Jump Statement- Continue

---
+++ A051_JumpStatement/Continue.java
@@ -0,0 +1,12 @@
+package A051_JumpStatement;
+
+public class Continue {
+    public static void main (String args[]){
+        for(int i=0; i<10; i++)
+        {
+            if(i==5)
+            continue;
+                System.out.println(i);
+            }
+    }
+}
