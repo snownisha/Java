d6efa590578f19efe5affaa936fe116391d8227d
snownisha <snownisha123@gmail.com>
2022-04-26 14:01:58 +0200

    Type Casting

---
+++ A054_TypeCasting/TypeCasting.java
@@ -0,0 +1,15 @@
+package A054_TypeCasting;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+    int a =33;
+    int b = 29;
+
+
+    double c = a;
+    double d = b;
+
+    System.out.println(c);
+    System.out.println(d);
+}
+}
