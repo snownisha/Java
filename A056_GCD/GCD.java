4b2e170fea3eabb306d7413d67e4e2d805a58d99
snownisha <snownisha123@gmail.com>
2022-05-15 21:53:27 +0200

    GCD

--- A056_GCD/GCD.java
+++ A056_GCD/GCD.java
@@ -2,20 +2,12 @@ package A056_GCD;
 
 public class GCD {
     public static void main(String[] args) {
-
-        // find GCD between n1 and n2
-        int n1 = 76, n2 = 13;
-
-        // initially set to gcd
+        int num1 = 76, num2 = 13;
         int gcd = 1;
-
-        for (int i = 1; i <= n1 && i <= n2; ++i) {
-
-            // check if i perfectly divides both n1 and n2
-            if (n1 % i == 0 && n2 % i == 0)
+        for (int i = 1; i <= num1 && i <= num2; ++i) {
+            if (num1 % i == 0 && num2 % i == 0)
                 gcd = i;
         }
-
-        System.out.println("GCD of " + n1 +" and " + n2 + " is " + gcd);
+        System.out.println("GCD of " + num1 +" and " + num2 + " is " + gcd);
     }
 }
