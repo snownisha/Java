56460fa3495860bb8467f32ece774a8ac5bb921d
snownisha <snownisha123@gmail.com>
2022-06-01 09:35:50 +0200

    Functions in Java

---
+++ A057_Function/Function.java
@@ -0,0 +1,14 @@
+package A057_Function;
+
+public class Function {
+    public static void main (String[] args) {
+
+        Names();
+    }
+
+
+        public static void Names() {
+            System.out.println("Snow Nisha");
+        }
+    }
+
