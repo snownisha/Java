a227269b2bbe961e286360cf139105d33a66f5ab
snownisha <snownisha123@gmail.com>
2022-06-02 05:29:10 +0200

    Labelled Loop

---
+++ A060_LabelledLoop/LabelledLoop.java
@@ -0,0 +1,18 @@
+package A060_LabelledLoop;
+
+public class LabelledLoop {
+    public static void main (String[] args) {
+        int i,j;
+        outerLoop:
+            for (i=1; i>=5;i++){
+        innerLoop:
+         for (j=1; j>=5;j++){
+             if(i==3 && j==3)
+                 break outerLoop;
+             System.out.println("*");
+         }
+         System.out.println("/n");
+
+            }
+    }
+}
